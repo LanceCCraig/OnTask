b4289cf0fc256d2c9c405883a83225f2d933cd11
Lance Craig <craiglc@mail.uc.edu>
2018-01-25 09:10:16 -0500

    Create hierarchy between event tables

--- External/Scripts/Initialize.sql
+++ External/Scripts/Initialize.sql
@@ -278,23 +278,6 @@ CREATE TABLE [dbo].[UserToken]
 )
 GO
 
-CREATE TABLE [dbo].[EventGroup]
-(
-    [Id]            INT             IDENTITY(1, 1)  NOT NULL,
-    [UserId]        NVARCHAR(450)   NOT NULL,
-    [Name]          NVARCHAR(500)   NOT NULL,
-    [DisplayName]   NVARCHAR(500)   NOT NULL,
-    [CreatedOn]     DATETIME        NOT NULL,
-    [UpdatedOn]     DATETIME        NULL,
-    CONSTRAINT [PK_EventGroup] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_EventGroup_User_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([Id])
-)
-GO
-
-CREATE NONCLUSTERED INDEX [IX_EventGroup_UserId]
-    ON [dbo].[EventGroup] ([UserId] ASC)
-GO
-
 CREATE TABLE [dbo].[EventParent]
 (
     [Id]			INT             IDENTITY(1, 1)  NOT NULL,
@@ -312,17 +295,46 @@ CREATE NONCLUSTERED INDEX [IX_EventParent_UserId]
     ON [dbo].[EventParent] ([UserId] ASC)
 GO
 
+CREATE TABLE [dbo].[EventGroup]
+(
+    [Id]            INT             IDENTITY(1, 1)  NOT NULL,
+    [EventParentId] INT             NOT NULL,
+    [UserId]        NVARCHAR(450)   NOT NULL,
+    [Name]          NVARCHAR(500)   NOT NULL,
+    [DisplayName]   NVARCHAR(500)   NOT NULL,
+    [CreatedOn]     DATETIME        NOT NULL,
+    [UpdatedOn]     DATETIME        NULL,
+    CONSTRAINT [PK_EventGroup] PRIMARY KEY CLUSTERED ([Id] ASC), 
+    CONSTRAINT [FK_EventGroup_EventParent_EventParentId] FOREIGN KEY ([EventParentId]) REFERENCES [dbo].[EventParent] ([Id]),
+    CONSTRAINT [FK_EventGroup_User_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([Id])
+)
+GO
+
+CREATE NONCLUSTERED INDEX [IX_EventGroup_EventParentId]
+    ON [dbo].[EventGroup] ([EventParentId] ASC)
+GO
+
+CREATE NONCLUSTERED INDEX [IX_EventGroup_UserId]
+    ON [dbo].[EventGroup] ([UserId] ASC)
+GO
+
 CREATE TABLE [dbo].[EventType]
 (
     [Id]			INT             IDENTITY(1, 1)  NOT NULL,
+    [EventGroupId]  INT             NOT NULL,
     [UserId]        NVARCHAR(450)	NOT NULL,
     [Name]          NVARCHAR(500)   NOT NULL,
     [DisplayName]   NVARCHAR(500)   NOT NULL,
     [CreatedOn]     DATETIME        NOT NULL,
     [UpdatedOn]     DATETIME        NULL,
     CONSTRAINT [PK_EventType] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_EventType_EventGroup_EventGroupId] FOREIGN KEY ([EventGroupId]) REFERENCES [dbo].[EventGroup] ([Id]),
     CONSTRAINT [FK_EventType_User_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([Id])
 )
+GO
+
+CREATE NONCLUSTERED INDEX [IX_EventType_EventGroupId]
+    ON [dbo].[EventType] ([EventGroupId] ASC)
 GO
 
 CREATE NONCLUSTERED INDEX [IX_EventType_UserId]
--- OnTask.Database/Tables/Event/EventGroup.sql
+++ OnTask.Database/Tables/Event/EventGroup.sql
@@ -1,14 +1,20 @@
 ﻿CREATE TABLE [dbo].[EventGroup]
 (
     [Id]            INT             IDENTITY(1, 1)  NOT NULL,
+    [EventParentId] INT             NOT NULL,
     [UserId]        NVARCHAR(450)   NOT NULL,
     [Name]          NVARCHAR(500)   NOT NULL,
     [DisplayName]   NVARCHAR(500)   NOT NULL,
     [CreatedOn]     DATETIME        NOT NULL,
     [UpdatedOn]     DATETIME        NULL,
-    CONSTRAINT [PK_EventGroup] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [PK_EventGroup] PRIMARY KEY CLUSTERED ([Id] ASC), 
+    CONSTRAINT [FK_EventGroup_EventParent_EventParentId] FOREIGN KEY ([EventParentId]) REFERENCES [dbo].[EventParent] ([Id]),
     CONSTRAINT [FK_EventGroup_User_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([Id])
 )
+GO
+
+CREATE NONCLUSTERED INDEX [IX_EventGroup_EventParentId]
+    ON [dbo].[EventGroup] ([EventParentId] ASC)
 GO
 
 CREATE NONCLUSTERED INDEX [IX_EventGroup_UserId]
