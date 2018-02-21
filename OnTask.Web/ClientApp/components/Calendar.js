22d644a560d737236ded0fdc4ad2607df0c06836
Home\Luke <shaferla@mail.uc.edu>
2018-02-21 16:13:45 -0500

    Added temp Calendar page

---
+++ OnTask.Web/ClientApp/components/Calendar.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+
+class Calendar extends React.Component {
+    render() {
+        return <div>
+            <h1>Calendar</h1>
+        
+            <p>This page is under construction! We'll be putting a calendar here soon!</p>
+
+        </div>;
+    }
+}
+
+export default Calendar;
