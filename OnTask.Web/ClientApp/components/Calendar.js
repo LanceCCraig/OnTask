e16b809e154bfc9cb9b21bf6ee5eced7a08f8956
snhendo <snhllama74@sbcglobal.net>
2018-02-21 18:10:43 -0500

    Trying to get BigCalendar to work
    Week and day seem to work but not month or agenda

--- OnTask.Web/ClientApp/components/Calendar.js
+++ OnTask.Web/ClientApp/components/Calendar.js
@@ -1,14 +1,50 @@
 import * as React from 'react';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
 import { RouteComponentProps } from 'react-router-dom';
+
+BigCalendar.momentLocalizer(moment);
 
 class Calendar extends React.Component {
     render() {
-        return <div>
+        return (
+        <div>
             <h1>Calendar</h1>
-        
             <p>This page is under construction! We'll be putting a calendar here soon!</p>
-
-        </div>;
+            <BigCalendar
+                events={
+                    [
+                        {
+                          id: 0,
+                          title: 'All Day Event very long title',
+                          allDay: true,
+                          start: new Date(2018, 1, 18),
+                          end: new Date(2018, 1, 1),
+                        },
+                        {
+                            id: 1,
+                            title: 'Long Event',
+                            start: new Date(2018, 1, 7),
+                            end: new Date(2018, 1, 10),
+                          },
+                        
+                          {
+                            id: 2,
+                            title: 'DTS STARTS',
+                            start: new Date(2018, 1, 13, 0, 0, 0),
+                            end: new Date(2018, 1, 20, 0, 0, 0),
+                          },
+                    ]
+                }
+                views={['month', 'week', 'day']}
+                step={60}
+                showMultiDayTimes
+                startAccessor="start"
+                endAccessor="end"
+                defaultDate={new Date(2018, 1, 21)}
+            />
+        </div>
+        )
     }
 }
 
